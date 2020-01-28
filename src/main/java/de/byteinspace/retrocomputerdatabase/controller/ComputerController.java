a0948934361a1a802d1836d1916e953bc3530983
Daniel M. Ludwig <contact@daniel-ludwig.net>
2020-01-28 21:31:41 +0100

    First layout from Thymleaf

--- src/main/java/de/byteinspace/retrocomputerdatabase/controller/ComputerController.java
+++ src/main/java/de/byteinspace/retrocomputerdatabase/controller/ComputerController.java
@@ -1,22 +1,24 @@
 package de.byteinspace.retrocomputerdatabase.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import de.byteinspace.retrocomputerdatabase.entity.Computer;
 import de.byteinspace.retrocomputerdatabase.repository.ComputerRepository;
 
 
-@RestController
+@Controller
 @RequestMapping("/computer")
 public class ComputerController {
 
 	@Autowired private ComputerRepository computerRepository;
 	
-	@RequestMapping(value = "", method = RequestMethod.GET)
+	@GetMapping("/all")
 	public Iterable<Computer> getAllComputer() {
 		return computerRepository.findAll();
 	}
@@ -26,4 +28,10 @@ public class ComputerController {
 		return computerRepository.findComputerByName(name);
 	}
 	
+	@GetMapping("/showall")
+	public String showALlForm(Model model) {
+		model.addAttribute("computers", computerRepository.findAll());
+		
+		return "showAll";
+	}
 }
